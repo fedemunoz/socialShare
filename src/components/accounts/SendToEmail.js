60fb1e421079da132bc7e265c4234418cf880233
Federico Munoz <fedemz88@gmail.com>
2021-04-24 13:08:47 -0300

    change regex for validator package

--- src/components/accounts/SendToEmail.js
+++ src/components/accounts/SendToEmail.js
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
-import AccountsContext from "../../context/accounts/accountsContext";
-import "./sendToEmail.scss";
-
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import validator from "validator";
+import AccountsContext from "../../context/accounts/accountsContext";
+import "./sendToEmail.scss";
 
 const SendToEmail = () => {
   const [buttonDisabled, setButtonDisabled] = React.useState(true);
@@ -15,16 +15,9 @@ const SendToEmail = () => {
 
   const onChange = (event) => {
     if (buttonDisabled) setButtonDisabled(false);
-    const value = event.target.value;
-    isEmail(value) ? setshowError(false) : setshowError(true);
-  };
-
-  const isEmail = (val) => {
-    let regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!regEmail.test(val)) {
-      return false;
-    }
-    return true;
+    validator.isEmail(event.target.value)
+      ? setshowError(false)
+      : setshowError(true);
   };
 
   return (
