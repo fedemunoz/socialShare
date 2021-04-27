87e5c5e0cb34d7160602da08bae3f9a733590282
Federico Munoz <fedemz88@gmail.com>
2021-04-27 18:02:38 -0300

    roboto font

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
+import "typeface-roboto";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
